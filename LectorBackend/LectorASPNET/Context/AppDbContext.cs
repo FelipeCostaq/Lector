3b62a98e192c922dac816acb448f636c19e110df
FelipeCostaq <felipecostasiqu@gmail.com>
2026-01-15 11:53:19 -0300

    :card_file_box: DbContext

---
+++ LectorBackend/LectorASPNET/Context/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LectorASPNET.Models;
+
+namespace LectorASPNET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users
+        {
+            get; set;
+        }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<UserLibrary> UserLibraries { get; set; }
+        public DbSet<Social> Socials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Social>()
+                .HasKey(s => new { s.FollowerId, s.FollowingId }); 
+
+            modelBuilder.Entity<Social>()
+                .HasOne(s => s.Follower)
+                .WithMany(u => u.Following)
+                .HasForeignKey(s => s.FollowerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Social>()
+                .HasOne(s => s.Following)
+                .WithMany(u => u.Followers)
+                .HasForeignKey(s => s.FollowingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Review>()
+                .Property(r => r.Rating)
+                .HasColumnType("double precision"); 
+        }
+    }
+}
