22cba8cae7ad3cadeb38a8526b63ad73cf806fca
FelipeCostaq <felipecostasiqu@gmail.com>
2026-01-21 16:56:31 -0300

    :construction_worker: Register and Login Data Transfer Object

---
+++ LectorBackend/LectorASPNET/DTO/RegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LectorASPNET.DTO
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
