e63c0852fc6764aed3a587e7eb0c4cd3fd876e79
FelipeCostaq <felipecostasiqu@gmail.com>
2026-01-21 17:45:31 -0300

    :sparkles: BooksController search

---
+++ LectorBackend/LectorASPNET/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LectorASPNET.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LectorASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly GoogleBooksService _googleBooksService;
+
+        public BooksController(GoogleBooksService googleBooksService)
+        {
+            _googleBooksService = googleBooksService;
+        }
+
+        [HttpGet("search")]
+        [Authorize]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return BadRequest("Por favor, digite o nome de um livro ou autor.");
+            }
+
+            var books = await _googleBooksService.SearchBooksAsync(query);
+
+            return Ok(books);
+        }
+    }
+}
